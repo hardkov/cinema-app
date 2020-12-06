5c44e4dbbc49145aba3f82286bd62292527233c2
Saollin <gregory.janosz@gmail.com>
2020-12-06 14:02:14 +0100

    Add Firebase configuration

---
+++ src/main/java/daos/FirestoreDatabase.java
@@ -0,0 +1,43 @@
+package daos;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+
+public class FirestoreDatabase {
+
+    private static final String projectId = "habeja-to-project";
+
+    Firestore db;
+
+    public FirestoreDatabase() throws URISyntaxException {
+        try {
+            FileInputStream serviceAccount =
+                    new FileInputStream("src/main/resources/habeja-to-project-firebase-adminsdk-q0xq4-14a5591339.json");
+            GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(credentials)
+                    .setProjectId(projectId)
+                    .build();
+            FirebaseApp.initializeApp(options);
+
+            this.db = FirestoreClient.getFirestore();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Firestore getDb() {
+        return db;
+    }
+}
