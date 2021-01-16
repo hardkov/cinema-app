b7cf799b5ea17ef4059b8793923f461720709492
hardkov <krzysztofH150@interia.pl>
2021-01-16 19:36:28 +0100

    Welcome message for employee and customer

--- src/main/java/controllers/CustomerPanelController.java
+++ src/main/java/controllers/CustomerPanelController.java
@@ -2,12 +2,29 @@ package controllers;
 
 import helpers.Redirect;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import model.User;
 import utils.Session;
 
 import javax.mail.search.ReceivedDateTerm;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class CustomerPanelController {
+public class CustomerPanelController implements Initializable {
     Class cls = getClass();
+
+    @FXML
+    public Label userWelcomeMessage;
+
+    public void initialize(URL url, ResourceBundle rb){
+        User currentUser = Session.getSession().getCurrentUser();
+        if(currentUser != null){
+            this.userWelcomeMessage.setText("Welcome, " + currentUser.getLogin());
+        }
+
+    }
 
     public void logout(ActionEvent event) {
         Session.getSession().setCurrentUser(null);
--- src/main/java/controllers/WorkerPanelController.java
+++ src/main/java/controllers/WorkerPanelController.java
@@ -2,10 +2,29 @@ package controllers;
 
 import helpers.Redirect;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import model.User;
 import utils.Session;
 
-public class WorkerPanelController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WorkerPanelController implements Initializable {
     Class cls = getClass();
+
+    @FXML
+    public Label userWelcomeMessage;
+
+    public void initialize(URL url, ResourceBundle rb){
+        User currentUser = Session.getSession().getCurrentUser();
+        if(currentUser != null){
+            this.userWelcomeMessage.setText("Welcome, " + currentUser.getLogin());
+        }
+
+    }
+
     public void screenings(ActionEvent event) {
         System.out.println("Screenings button pressed");
     }
