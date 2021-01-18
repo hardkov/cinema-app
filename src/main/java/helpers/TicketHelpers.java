146240c657ed08c6ff06d9d99c39df9fbf136ff4
hardkov <krzysztofh150@interia.pl>
2021-01-19 00:13:50 +0100

    renamed class with ticket helper functions

--- src/main/java/helpers/TicketHelpers.java
+++ src/main/java/helpers/TicketHelpers.java
@@ -7,7 +7,7 @@ import model.Ticket;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class TicketUtils {
+public class TicketHelpers {
     public static int getFreeSeatId(Screening screening){
         TicketDao ticketDao = new TicketDao();
         List<Ticket> allTickets = ticketDao.getAllTickets();
