d1311bbbbac7e3d2a8577139ca1bcab8535a30d7
hardkov <krzysztofh150@interia.pl>
2021-01-18 22:22:01 +0100

    added distinct constrain on discounts

--- src/main/java/validators/DiscountNameValidator.java
+++ src/main/java/validators/DiscountNameValidator.java
@@ -1,15 +1,23 @@
 package validators;
 
+import daos.DiscountDao;
 import model.Discount;
 
 import java.util.List;
 
 public class DiscountNameValidator implements IValidator<Discount>{
+    DiscountDao discountDao = new DiscountDao();
+
     @Override
     public boolean isValid(Discount obj, List<String> feedback) {
         String name = obj.getName();
         if(name.length() < 1){
             if(feedback != null) feedback.add("Discount name must be at least one character long");
+            return false;
+        }
+
+        if(discountDao.doesDiscountExist(name)){
+            if(feedback != null) feedback.add("Discount with that name already exists");
             return false;
         }
 
