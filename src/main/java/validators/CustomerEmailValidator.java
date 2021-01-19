2ad3c71dec46c134aca3349ddd8b7dec5d6061f9
hardkov <krzysztofh150@interia.pl>
2021-01-19 03:53:53 +0100

    added email validation

--- src/main/java/validators/CustomerEmailValidator.java
+++ src/main/java/validators/CustomerEmailValidator.java
@@ -2,6 +2,9 @@ package validators;
 
 import model.Customer;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import java.util.Calendar;
 import java.util.List;
 
 public class CustomerEmailValidator implements IValidator<Customer> {
@@ -11,6 +14,15 @@ public class CustomerEmailValidator implements IValidator<Customer> {
             if(feedback != null) feedback.add("Email has to be at lest 1 character long");
             return false;
         }
+
+        try{
+            InternetAddress emailAddress = new InternetAddress(obj.getEmail());
+            emailAddress.validate();
+        } catch (AddressException e){
+            if(feedback != null) feedback.add("Email is not valid");
+            return false;
+        }
+
         return true;
     }
 }
