d5254322d5f2bae363a1a792b32e617831dff55d
Saollin <gregory.janosz@gmail.com>
2021-01-19 12:24:52 +0100

    Add recommended movie for customer

--- src/main/java/controllers/CustomerPanelController.java
+++ src/main/java/controllers/CustomerPanelController.java
@@ -7,9 +7,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import model.Screening;
-import model.Ticket;
-import model.User;
+import model.*;
+import statistics.PopularityMovieStatisticCreator;
 import utils.Session;
 
 import javax.mail.search.ReceivedDateTerm;
@@ -22,12 +21,34 @@ public class CustomerPanelController implements Initializable {
     @FXML
     public Label userWelcomeMessage;
 
+    @FXML
+    public Label recommendedMovieHeaderLabel;
+
+    @FXML
+    public Label recommendMovieLabel;
+
+    private final static String headerForRecommendedMovie = "Recommended movie for you:";
+    private final static String headerForLackOfRecommendedMovie = "Wow! You've seen all our movies! <3";
+
     public void initialize(URL url, ResourceBundle rb){
         User currentUser = Session.getSession().getCurrentUser();
         if(currentUser != null){
             this.userWelcomeMessage.setText("Welcome, " + currentUser.getLogin());
         }
+        setRecommendedMovieForCustomer(currentUser);
+    }
 
+    private void setRecommendedMovieForCustomer(User user) {
+        Customer customer = (Customer) user;
+        PopularityMovieStatisticCreator creator = new PopularityMovieStatisticCreator();
+        Movie recommendedMovie = creator.getMoviePropositionForCustomer(customer);
+        if (recommendedMovie == null) {
+            recommendMovieLabel.setText("");
+            recommendedMovieHeaderLabel.setText(headerForLackOfRecommendedMovie);
+        } else {
+            recommendMovieLabel.setText(recommendedMovie.getTitle());
+            recommendedMovieHeaderLabel.setText(headerForRecommendedMovie);
+        }
     }
 
     public void logout(ActionEvent event) {
