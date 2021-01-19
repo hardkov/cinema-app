adfa3d137b2316e656edba4a6d6b41e3df30e73e
hardkov <krzysztofh150@interia.pl>
2021-01-19 02:28:36 +0100

    Moved validating hall id to its validators

--- src/main/java/validators/HallIdValidator.java
+++ src/main/java/validators/HallIdValidator.java
@@ -9,6 +9,10 @@ public class HallIdValidator implements IValidator<Hall>{
     HallDao hallDao = new HallDao();
     @Override
     public boolean isValid(Hall obj, List<String> feedback) {
+        if(obj.getHallId() < 0){
+            if(feedback != null) feedback.add("Invalid hall id");
+            return false;
+        }
 
         if(hallDao.doesHallExist(obj.getHallId())){
             if(feedback != null) feedback.add("Hall with this id exists");
